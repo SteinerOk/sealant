5bb4e66437b534ef80591b1d31d9e1146247ebf6
Ihor Kushnirenko <ihor.kushnirenko@gmail.com>
2026-04-24 18:39:33 +0300

    Refactor ChildScopesSymbolProcessor to improve ViewModel scope marker generation and enhance code readability

--- core-compiler-ksp/src/main/java/dev/steinerok/sealant/core/compiler/ksp/ChildScopesSymbolProcessor.kt
+++ core-compiler-ksp/src/main/java/dev/steinerok/sealant/core/compiler/ksp/ChildScopesSymbolProcessor.kt
@@ -24,8 +24,12 @@ import com.google.devtools.ksp.processing.SymbolProcessorEnvironment
 import com.google.devtools.ksp.processing.SymbolProcessorProvider
 import com.google.devtools.ksp.symbol.KSAnnotated
 import com.google.devtools.ksp.symbol.KSClassDeclaration
+import com.google.devtools.ksp.symbol.KSFile
+import com.google.devtools.ksp.validate
+import com.squareup.kotlinpoet.ClassName
 import com.squareup.kotlinpoet.FileSpec
 import com.squareup.kotlinpoet.KModifier
+import com.squareup.kotlinpoet.TypeSpec
 import com.squareup.kotlinpoet.ksp.addOriginatingKSFile
 import com.squareup.kotlinpoet.ksp.toClassName
 import com.squareup.kotlinpoet.ksp.writeTo
@@ -43,17 +47,6 @@ import dev.steinerok.sealant.compiler.ksp.requireContainingFile
  * Description of the ViewModel scope marker generation.
  * This generator creates an abstract class that acts as a namespace or
  * structural marker for ViewModel injection within a specific Dagger/Anvil scope.
- *
- * Should generate the following component:
- *
- * 1. ViewModel Scope Marker Class:
- * An abstract class with a private constructor, ensuring it cannot be instantiated.
- * It serves as a foundational type or namespace in the generated code to logically
- * group, attach, or identify ViewModel multibindings and factories associated
- * with the target `<Scope>`.
- * ```
- * public abstract class ViewModel_<Scope> private constructor()
- * ```
  */
 public class ChildScopesSymbolProcessor(
     private val codeGenerator: CodeGenerator,
@@ -62,40 +55,64 @@ public class ChildScopesSymbolProcessor(
 ) : SymbolProcessor {
 
     override fun process(resolver: Resolver): List<KSAnnotated> {
-        resolver
+        val (validSymbols, invalidSymbols) = resolver
             .getSymbolsWithAnnotation(ClassNames.sealantConfiguration)
             .filterIsInstance<KSClassDeclaration>()
-            .onEach { _ ->  /* Verification if you need */ }
+            .partition { symbol -> symbol.validate() }
+
+        validSymbols
             .forEach { symbol ->
                 generateByProcessor(symbol)?.writeTo(
                     codeGenerator = codeGenerator,
                     aggregating = false,
                 )
             }
-        return emptyList()
+
+        return invalidSymbols
     }
 
     private fun generateByProcessor(clazz: KSClassDeclaration): FileSpec? {
         val hasViewModelSupport = clazz.hasSealantFeatureForScope(SealantFeature.ViewModel)
         if (!hasViewModelSupport) return null
-        //
-        val packageName = clazz.packageName.asString()
-        val fileName = clazz.simpleName.asString() + "_SealantChildScopes"
-        val vmScopeClassName = buildVmScopeClassName(clazz.toClassName())
-        //
-        val content = SealantFileSpec(packageName, fileName) {
-            val vmScopeClass = ClassSpec(vmScopeClassName) {
-                addModifiers(KModifier.ABSTRACT)
-                primaryConstructor(
-                    ConstructorSpec {
-                        addModifiers(KModifier.PRIVATE)
-                    }
-                )
-                addOriginatingKSFile(clazz.requireContainingFile())
-            }
-            addType(vmScopeClass)
+
+        val origClassName = clazz.toClassName()
+
+        val fileName = "${origClassName.simpleName}_SealantChildScopes"
+        val fileNode = clazz.requireContainingFile()
+
+        return SealantFileSpec(origClassName.packageName, fileName) {
+            // Генерируем ViewModel Scope Marker Class
+            addType(buildVmScopeMarkerClass(origClassName, fileNode))
         }
-        return content
+    }
+
+    /**
+     * Generates the ViewModel Scope Marker Class.
+     * * An abstract class with a private constructor, ensuring it cannot be instantiated.
+     * It serves as a foundational type or namespace in the generated code to logically
+     * group, attach, or identify ViewModel multibindings and factories associated
+     * with the target `<Scope>`.
+     * * Output example:
+     * ```kotlin
+     * public abstract class ViewModel_<Scope> private constructor()
+     * ```
+     */
+    private fun buildVmScopeMarkerClass(
+        origClassName: ClassName,
+        fileNode: KSFile,
+    ): TypeSpec {
+        val vmScopeClassName = buildVmScopeClassName(origClassName)
+
+        return ClassSpec(vmScopeClassName) {
+            addModifiers(KModifier.ABSTRACT)
+            primaryConstructor(
+                ConstructorSpec {
+                    addModifiers(KModifier.PRIVATE)
+                }
+            )
+
+            addOriginatingKSFile(fileNode)
+        }
     }
 
     /**
@@ -104,7 +121,6 @@ public class ChildScopesSymbolProcessor(
     @Suppress("unused")
     @AutoService(SymbolProcessorProvider::class)
     public class Provider : SymbolProcessorProvider {
-
         override fun create(environment: SymbolProcessorEnvironment): SymbolProcessor {
             return ChildScopesSymbolProcessor(
                 codeGenerator = environment.codeGenerator,
